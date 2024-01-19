a2bd80fc29e3fa9c6fa58f0aec640685471b2c40
AbdullahEtf <155556575+AbdullahEtf@users.noreply.github.com>
2024-01-19 10:18:31 +0100

    Revert "Fix view update"

--- src/ViewActivity.cpp
+++ src/ViewActivity.cpp
@@ -567,10 +567,8 @@ void ViewActivity::SetCurrentSubject() {
         td::Variant id = _id.getValue();
         td::Variant point = _points.getValue();
         td::Decimal2 x = point.dec2Val();
-        //showAlert(tr("succes"), tr("succesEE"));
         if (x > 10000)
         {
-            showAlert(tr("failure"), tr("alertOver100"));
             return false;
         }
 
@@ -595,11 +593,8 @@ void ViewActivity::SetCurrentSubject() {
         if (!pSelect->moveNext())
             return false;
 
-        if (sum + x >= 10000) {
-            showAlert(tr("failure"), tr("alertOver100"));
+        if (sum + x > 10000)
             return false;
-        }
-
 
         return true;
     }
