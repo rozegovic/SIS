4ef2ea71a7f969c232cb73c46f1ea23c3dcad833
Lamija123 <155556791+Lamija123@users.noreply.github.com>
2024-01-19 12:28:05 +0100

    popravljenje funkcije populateDataForTable1 i 2 da razdvoje predane i ne predane zadace

--- src/upload.cpp
+++ src/upload.cpp
@@ -110,13 +110,13 @@ void ViewUpload::SetCurrentData() {
 
 void ViewUpload::populateDataForTable1()
 {
-    _pDS = dp::getMainDatabase()->createDataSet("SELECT pr.ID_Predaje, P.Naziv_Predmeta, P.Sifra_Predmeta, A.Naziv_Aktivnosti, r.Datum_Predaje, r.Vrijeme_Predaje, P.ID_Predmeta FROM Predmet P, Aktivnosti A, Predaja pr, OpenPredaja r where P.ID_Predmeta = A.ID_Predmeta and r.ID_Aktivnosti = A.ID_Aktivnosti and (A.Tip_Aktivnosti = 2 or A.Tip_Aktivnosti = 5) and pr.Predano = 0 and r.ID = pr.ID_OpenPredaja", dp::IDataSet::Execution::EX_MULT);
-    dp::DSColumns cols(_pDS->allocBindColumns(7));
-    cols << "ID_Predaje" << td::int4
-        << "Naziv_Predmeta" << td::string8
+
+    _pDS = dp::getMainDatabase()->createDataSet("SELECT P.Naziv_Predmeta, P.Sifra_Predmeta, A.Naziv_Aktivnosti, op.Datum_Predaje, op.Vrijeme_Predaje, P.ID_Predmeta FROM Predmet P, Aktivnosti A, Predaja pr, OpenPredaja op where P.ID_Predmeta = A.ID_Predmeta and (A.Tip_Aktivnosti = 2 or A.Tip_Aktivnosti = 5) and op.ID = pr.ID_OpenPredaja and pr.Predano = 0 and pr.ID_Aktivnosti = A.ID_Aktivnosti", dp::IDataSet::Execution::EX_MULT);
+    dp::DSColumns cols(_pDS->allocBindColumns(6));
+    cols << "Naziv_Predmeta" << td::string8 
         << "Sifra_Predmeta" << td::string8 
         << "Naziv_Aktivnosti" << td::string8 
-        << "Datum_Predaje" << td::date
+        << "Datum_Predaje" << td::date 
         << "Vrijeme_Predaje" << td::time
         /* << "Reg_time" << td::time */ 
         << "ID_Predmeta" << td::int4;;
@@ -131,15 +131,12 @@ void ViewUpload::populateDataForTable1()
 }
 void ViewUpload::populateDataForTable2() // treba modifikovati select da radi kad se preda na zadnji dan da se uporedi vrijeme 
 {
-    _pDS2 = dp::getMainDatabase()->createDataSet("SELECT P.Naziv_Predmeta, P.Sifra_Predmeta, A.Naziv_Aktivnosti, r.Datum_Predaje, r.Vrijeme_Predaje, P.ID_Predmeta, pr.Datoteka FROM Predmet P, Aktivnosti A, Predaja pr, OpenPredaja r where P.ID_Predmeta = A.ID_Predmeta and r.ID_Aktivnosti = A.ID_Aktivnosti and (A.Tip_Aktivnosti = 2 or A.Tip_Aktivnosti = 5) and pr.Predano = 1 and r.ID = pr.ID_OpenPredaja", dp::IDataSet::Execution::EX_MULT); 
-    dp::DSColumns cols(_pDS2->allocBindColumns(7));
-    cols << "Naziv_Predmeta" << td::string8
-        << "Sifra_Predmeta" << td::string8
-        << "Naziv_Aktivnosti" << td::string8
-        << "Datum_Predaje" << td::date
-        << "Vrijeme_Predaje" << td::time
-        << "ID_Predmeta" << td::int4
-        << "Datoteka" << td::string8; // treba vidjet koji tip podatka treba za datoteku
+
+ // treba vidjet koji tip podatka treba za datoteku
+
+    _pDS2 = dp::getMainDatabase()->createDataSet("SELECT P.Naziv_Predmeta, P.Sifra_Predmeta, A.Naziv_Aktivnosti, op.Datum_Predaje, op.Vrijeme_Predaje, P.ID_Predmeta, pr.NazivFajla, pr.Datoteka FROM Predmet P, Aktivnosti A, Predaja pr, OpenPredaja op where P.ID_Predmeta = A.ID_Predmeta and (A.Tip_Aktivnosti = 2 or A.Tip_Aktivnosti = 5) and op.ID = pr.ID_OpenPredaja and pr.Predano = 1 and pr.ID_Aktivnosti = A.ID_Aktivnosti", dp::IDataSet::Execution::EX_MULT);
+    dp::DSColumns cols(_pDS2->allocBindColumns(8));
+    cols << "Naziv_Predmeta" << td::string8 << "Sifra_Predmeta" << td::string8 << "Naziv_Aktivnosti" << td::string8 << "Datum_Predaje" << td::date << "Vrijeme_Predaje" << td::time << "ID_Predmeta" << td::int4 << "NazivFajla" << td::string8 << "Datoteka" << td::string8; // treba vidjet koji tip podatka treba za datoteku
 
     if (!_pDS2->execute())
     {
