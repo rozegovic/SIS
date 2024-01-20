8780a32eca17f76a5ca6410076f118375e809d14
EminaKovacevic <ekovacevic2@etf.unsa.ba>
2024-01-20 20:04:02 +0100

    fiks transakcije

--- src/Reports.cpp
+++ src/Reports.cpp
@@ -13,6 +13,11 @@
 
 void examAttendance(const gui::Image* pImage, td::INT4 SubjectID)
 {	
+	dp::IStatementPtr pSelect2 = dp::getMainDatabase()->createStatement("INSERT INTO PolazniciAktivnosti (ID_Korisnika, ID_Aktivnosti) SELECT a.ID_Studenta, b.ID_Aktivnosti FROM Prijavljeni_ispiti a JOIN Rokovi b ON a.ID_Roka = b.ID_Roka WHERE NOT EXISTS ( SELECT 1 FROM PolazniciAktivnosti pa WHERE pa.ID_Korisnika = a.ID_Studenta AND pa.ID_Aktivnosti = b.ID_Aktivnosti )");
+	dp::Transaction a(dp::getMainDatabase());
+	if (!pSelect2->execute())
+		return;
+	a.commit();
 	//------------------TAKODER dodati select za upis u tabelu PolazniciAktivnosti - samo se ucitava nakon sto je proslo vrijeme prijave ispita
 	dp::IDatabase* pDB = dp::getMainDatabase();
 	td::String name = "Studenti prijavljeni na ispit";
