3ef58800f3781e8f685eb58654c78fadaf29bd32
abudabiduba <agilic1@etf.unsa.ba>
2024-02-09 23:58:02 +0100

    Update ChatsForTStaff.h

--- src/ChatsForTStaff.h
+++ src/ChatsForTStaff.h
@@ -23,12 +23,28 @@ public:
     void onDraw(const gui::Rect& rect) override {
         gui::Size sz;
         getSize(sz);
-        gui::Point cp(sz.width / 2, sz.height / 2);
-        td::INT4 x = cp.x;
-        td::INT4 y = cp.y;
+        //gui::Point cp(sz.width / 2, sz.height / 2);
+        //td::INT4 x = cp.x;
+        //td::INT4 y = cp.y
 
-        gui::Rect imgRect(x - 15 - x / 4, y - 15 - y / 4, x + 15 + x / 4, y + 15 + y / 4);
-        _etf.draw(imgRect, gui::Image::AspectRatio::Keep, td::HAlignment::Center, td::VAlignment::Center); //no
+        //gui::Rect imgRect(x - 15 - x / 4, y - 15 - y / 4, x + 15 + x / 4, y + 15 + y / 4);
+        //_etf.draw(imgRect, gui::Image::AspectRatio::Keep, td::HAlignment::Center, td::VAlignment::Center); //no
+
+
+        _pDS = dp::getMainDatabase()->createDataSet("SELECT Korisnici.ID as IDUser, Korisnici.Ime as nameUser, Korisnici.Prezime as surnameUser, Pozicija.ID as positionID, Pozicija.Naziv as roleUser, Korisnici.JMBG as jmbgUser, Korisnici.DatumUpisa as dateEUser, Korisnici.Adresa as addressUser, Korisnici.DatumRodjenja as dateBUser, Korisnici.Indeks as indeksUser FROM Korisnici, Pozicija WHERE Korisnici.PozicijaID = Pozicija.ID AND Korisnici.ID>0", dp::IDataSet::Execution::EX_MULT);
+        //ovo treba da se popuni unutrasnjost pravougaonika imenom.
+
+
+        gui::Rect imgRect(0, 0, sz.width, 100);
+        _etf.draw(imgRect, gui::Image::AspectRatio::Keep, td::HAlignment::Left); //no
+        _etf.draw(imgRect, gui::Image::AspectRatio::Keep, td::HAlignment::Right); //no
+        gui::Shape::drawRect(imgRect, td::ColorID::Blue, 2, td::LinePattern::Dot);
+
+        for (int i = 0; i < 3; i++) {
+            imgRect.translate(0, 110);
+            _etf.draw(imgRect, gui::Image::AspectRatio::Keep, td::HAlignment::Left);
+            gui::Shape::drawRect(imgRect, td::ColorID::Red, 5, td::LinePattern::DashEq);
+        }
 
 
     };
